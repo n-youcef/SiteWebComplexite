17821c552616de9d47fce106734932168de12ef6
aziz <abdoulaziz2512@gmail.com>
2019-05-24 05:34:19 +0200

    ajout d'exemples 6

--- js/Js_Clique/ExemplesClique.js
+++ js/Js_Clique/ExemplesClique.js
@@ -67,27 +67,27 @@ function afficherReponseClique(numReponse, reponsePropose){
     switch (numReponse){
         case 1:
             if(reponsePropose == "OUI")
-                reponseClique.innerHTML = "Juste: on voit bien les sommets 2,4 et 5 qui forment un triangle dans le graphe";
+                reponseClique.innerHTML = "Reponse Juste: on voit bien les sommets 2,4 et 5 qui forment un triangle dans le graphe";
             else
-                reponseClique.innerHTML = "Faux: provisoire: C'est VRAI";
+                reponseClique.innerHTML = "Reponse Faux";
             break;
         case 2:
             if(reponsePropose == "OUI")
-                reponseClique.innerHTML = "provisoire: C'est FAUXX";
+                reponseClique.innerHTML = "Repone Faux car il n,existe pas de sous graphe complet de taille 4 dans ce graphe";
             else
-                reponseClique.innerHTML = "provisoire: C'est FAUX";
+                reponseClique.innerHTML = "Repone  Vrai";
             break;
         case 3:
             if(reponsePropose == "OUI")
-                reponseClique.innerHTML = "provisoire: C'est FAUXX";
+                reponseClique.innerHTML = "Reponse faux justification on a un graphe de 5 sommet non complet impossible d'avoir un clique de taille 5";
             else
-                reponseClique.innerHTML = "provisoire: C'est FAUX";
+                reponseClique.innerHTML = "Reponse Vrai";
             break;
         case 4:
             if(reponsePropose == "OUI")
-                reponseClique.innerHTML = "provisoire: C'est VRAII";
+                reponseClique.innerHTML = "Reponse Juste: on voit bien les sommets 1,2,3 et 4 qui forment un sous graphe complet dans le graphe";
             else
-                reponseClique.innerHTML = "provisoire: C'est VRAI";
+                reponseClique.innerHTML = "Reponse Faux";
             break;
 
     }
