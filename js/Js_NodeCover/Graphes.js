17dad9da2286ccf8778333d14a3ea30a39c21eb8
youcef.nazef <youcef.nazef@yahoo.com>
2019-05-24 03:44:56 +0200

    Ajout d'exemples

---
+++ js/Js_NodeCover/Graphes.js
@@ -0,0 +1,74 @@
+// Tableau contenant les noeuds
+var nodesG1Clique = new vis.DataSet([
+    {id: 1, label: '1'},
+    {id: 2, label: '2'},
+    {id: 3, label: '3'},
+    {id: 4, label: '4'},
+    {id: 5, label: '5'},
+]);
+
+// Tableau contenant les aretes
+var edgesG1Clique = new vis.DataSet([
+    {from: 1, to: 2},
+    {from: 1, to: 3},
+    {from: 3, to: 4},
+    {from: 2, to: 4},
+    {from: 2, to: 5},
+    {from: 4, to: 5},
+]);
+
+
+
+
+// Tableau contenant les noeuds
+var nodesG2Clique = new vis.DataSet([
+    {id: 1, label: '1'},
+    {id: 2, label: '2'},
+    {id: 3, label: '3'},
+    {id: 4, label: '4'},
+    {id: 5, label: '5'},
+]);
+
+// Tableau contenant les aretes
+var edgesG2Clique = new vis.DataSet([
+    {from: 1, to: 2},
+    {from: 1, to: 3},
+    {from: 1, to: 4},
+    {from: 3, to: 4},
+    {from: 2, to: 3},
+    {from: 2, to: 4},
+    {from: 2, to: 5},
+    {from: 4, to: 5},
+]);
+
+
+
+
+// Options pour le bon affichage des graphes
+// Script utilisé: vis.js (script téléchargé sur internet)
+// Une documentation est disponible sur le Web
+// http://visjs.org/docs/network/
+var options = {
+    
+    clickToUse: true,
+    
+    interaction:{
+        dragNodes:false, //Pour eviter que les noeuds bougent
+        dragView: false, //Pour eviter que le graphe bouge
+        zoomView: false,
+    },
+    
+    nodes:{
+        shape: 'circle', //Pour avoir des noeuds sous forme de cercle.
+    },
+
+    edges: {
+        smooth: { //Pour avoir des lignes biens droites
+            type: "continuous",
+            forceDirection: "none",
+            roundness: 1
+        }
+    }
+    
+    
+};
